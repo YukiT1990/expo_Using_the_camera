e6b75c2898a5985fe68a9931bb9c6b40873a3b1d
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2022-07-13 23:17:23 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={Camera.Constants.Type.back}>
+      </Camera>
     </View>
   );
 }
@@ -16,5 +35,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    width: "100%",
+    height: "100%"
   },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# expo_Using_the_camera
+
+https://snack.expo.dev/@yuki1990ex/8-104-hack-it-using-the-camera
